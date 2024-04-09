73b8235d93a49248998307a96c07c2849b772509
Ben Orchard <thefirstmuffinman@gmail.com>
2024-04-10 00:59:14 +0100

    fix GCstrLen missing generic meta instance

--- src/Bytezap/Common/Generic.hs
+++ src/Bytezap/Common/Generic.hs
@@ -8,6 +8,7 @@ import Data.Kind
 import DeFun.Core ( type (~>), type (@@) )
 
 type family GCstrLen (toLen :: Type ~> Natural) (gf :: k -> Type) :: Natural where
-    GCstrLen _     U1         = 0
-    GCstrLen toLen (K1 i c)   = toLen @@ c
-    GCstrLen toLen (l :*: r)  = GCstrLen toLen l + GCstrLen toLen r
+    GCstrLen _     U1          = 0
+    GCstrLen toLen (K1 i c)    = toLen @@ c
+    GCstrLen toLen (l :*: r)   = GCstrLen toLen l + GCstrLen toLen r
+    GCstrLen toLen (M1 _ _ gf) = GCstrLen toLen gf
